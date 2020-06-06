44df6bb972fd7301a38b86d127897c098a3078c5
FACTS  LIMITED <admin@factsafrica.com>
2020-06-06 08:13:06 +0300

    clean up with useEffect

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import IngredientForm from './IngredientForm';
 import Search from './Search';
 import IngredientList from './IngredientList';
@@ -6,19 +6,61 @@ import IngredientList from './IngredientList';
 const Ingredients = () => {
   const [ingredients, setIngredients] = useState([]);
 
-  const addIngredientHandler = (ingredient) => (
-      setIngredients(prevIngredients=>
-      [...prevIngredients, { id:Math.random().toString(), ...ingredient }]
-    )
-  );
+  useEffect(() => {
+    fetch('https://react-hooks-demo-7a4ec.firebaseio.com/ingredients.json')
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseData) => {
+        const loadedIngredients = [];
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount,
+          });
+        }
+        setIngredients(loadedIngredients);
+      });
+  }, []);
+
+  const addIngredientHandler = (ingredient) => {
+    fetch('https://react-hooks-demo-7a4ec.firebaseio.com/ingredients.json', {
+      method: 'POST',
+      body: JSON.stringify(ingredient),
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseData) => {
+        setIngredients((prevIngredients) => [
+          ...prevIngredients,
+          { id: responseData.name, ...ingredient },
+        ]);
+      });
+  };
+
+  const removeIngredientHandler = (id) => {
+    setIngredients((prevIngredients) =>
+      prevIngredients.filter((ingredient) => ingredient.id !== id)
+    );
+  };
+
+  const filteredIngredientsHandler = useCallback(filteredIngredient => {
+    setIngredients(filteredIngredient)
+  }, [])
 
   return (
     <div className="App">
       <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
-        <Search />
-        <IngredientList ingredients={ingredients} onRemoveItem={()=>{}} />
+        <Search onLoadIngredients={filteredIngredientsHandler}/>
+        <IngredientList
+          ingredients={ingredients}
+          onRemoveItem={removeIngredientHandler}
+        />
       </section>
     </div>
   );
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,15 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
 
-const Search = React.memo(props => {
+const Search = React.memo((props) => {
+  const { onLoadIngredients } = props;
+  const [enteredFilter, setEnteredFilter] = useState('');
+  const inputRef = useRef();
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (enteredFilter === inputRef.current.value) {
+        const query =
+          enteredFilter.length === 0
+            ? ''
+            : `?orderBy="title"&equalTo="${enteredFilter}"`;
+
+        fetch(
+          `https://react-hooks-demo-7a4ec.firebaseio.com/ingredients.json${query}`
+        )
+          .then((response) => {
+            return response.json();
+          })
+          .then((responseData) => {
+            const loadedIngredients = [];
+            for (const key in responseData) {
+              loadedIngredients.push({
+                id: key,
+                title: responseData[key].title,
+                amount: responseData[key].amount,
+              });
+            }
+            onLoadIngredients(loadedIngredients);
+          });
+      }
+    }, 500);
+  }, [enteredFilter, onLoadIngredients, inputRef]);
+
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" />
+          <input
+            ref={inputRef}
+            type="text"
+            value={enteredFilter}
+            onChange={(e) => setEnteredFilter(e.target.value)}
+          />
         </div>
       </Card>
     </section>
