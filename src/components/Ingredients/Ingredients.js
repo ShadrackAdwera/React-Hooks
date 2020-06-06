dcfc25fa7abb861bc9496698555c1455fe64855d
FACTS  LIMITED <admin@factsafrica.com>
2020-06-06 08:51:30 +0300

    load errors and state batching

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -2,13 +2,18 @@ import React, { useState, useEffect, useCallback } from 'react';
 import IngredientForm from './IngredientForm';
 import Search from './Search';
 import IngredientList from './IngredientList';
+import ErrorModal from '../UI/ErrorModal'
 
 const Ingredients = () => {
   const [ingredients, setIngredients] = useState([]);
+  const [isLoading, setIsLoading] = useState(false)
+  const [errorFetch, setErrorFetch] = useState()
 
   useEffect(() => {
+    setIsLoading(true)
     fetch('https://react-hooks-demo-7a4ec.firebaseio.com/ingredients.json')
       .then((response) => {
+        setIsLoading(false)
         return response.json();
       })
       .then((responseData) => {
@@ -21,16 +26,20 @@ const Ingredients = () => {
           });
         }
         setIngredients(loadedIngredients);
+      }).catch(error=>{
+        setErrorFetch(error.message)
       });
   }, []);
 
   const addIngredientHandler = (ingredient) => {
+    setIsLoading(true)
     fetch('https://react-hooks-demo-7a4ec.firebaseio.com/ingredients.json', {
       method: 'POST',
       body: JSON.stringify(ingredient),
       headers: { 'Content-Type': 'application/json' },
     })
       .then((response) => {
+        setIsLoading(false)
         return response.json();
       })
       .then((responseData) => {
@@ -38,19 +47,27 @@ const Ingredients = () => {
           ...prevIngredients,
           { id: responseData.name, ...ingredient },
         ]);
+      }).catch(error=>{
+        setErrorFetch(true)
+        setIsLoading(false)
       });
   };
 
   const removeIngredientHandler = (id) => {
+    setIsLoading(true)
     fetch(
       `https://react-hooks-demo-7a4ec.firebaseio.com/ingredients/${id}.json`,
       {
         method: 'DELETE',
       }
     ).then((response) => {
+      setIsLoading(false)
       setIngredients((prevIngredients) =>
         prevIngredients.filter((ingredient) => ingredient.id !== id)
       );
+    }).catch(error=>{
+      setIsLoading(false)
+      setErrorFetch(error.message)
     });
   };
 
@@ -58,9 +75,14 @@ const Ingredients = () => {
     setIngredients(filteredIngredient);
   }, []);
 
+  const clearError = () => {
+    setErrorFetch(null)
+  }
+
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={addIngredientHandler} />
+      {errorFetch? <ErrorModal onClose={clearError}>{errorFetch}</ErrorModal> : null}
+      <IngredientForm onAddIngredient={addIngredientHandler} loading={isLoading} />
 
       <section>
         <Search onLoadIngredients={filteredIngredientsHandler} />
